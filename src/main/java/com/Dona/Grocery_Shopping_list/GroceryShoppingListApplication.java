ef12f3f7f18c6ea87973fe1af1996bd9faaf01b9
Metehan Gultekin <mgmetehan@hotmail.com>
2022-04-23 20:22:01 +0300

    add mysql connection

---
+++ src/main/java/com/Dona/Grocery_Shopping_list/GroceryShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.Dona.Grocery_Shopping_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryShoppingListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dona/Grocery_Shopping_list/GroceryShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dona.Grocery_Shopping_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
