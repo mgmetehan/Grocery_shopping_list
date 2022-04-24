3855b6f1e0458a77baba5bbdb9066dd56fe5b29c
Metehan Gultekin <mgmetehan@hotmail.com>
2022-04-24 09:24:16 +0300

    Controller class is returning ResponseEntity

--- src/main/java/com/Dona/Grocery_Shopping_list/controller/ShoppingProductController.java
+++ src/main/java/com/Dona/Grocery_Shopping_list/controller/ShoppingProductController.java
@@ -4,6 +4,8 @@ package com.Dona.Grocery_Shopping_list.controller;
 import com.Dona.Grocery_Shopping_list.model.ShoppingProduct;
 import com.Dona.Grocery_Shopping_list.service.ShoppingProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,42 +19,42 @@ public class ShoppingProductController {
     private ShoppingProductService shoppingProductService;
 
     @GetMapping("/")
-    public String viewHomePage(Model model) {
+    public ResponseEntity<String> viewHomePage(Model model) {
         // display list of product
         model.addAttribute("listProducts", shoppingProductService.getAllProducts());
-        return "index";
+        return new ResponseEntity<>("index", HttpStatus.OK);
     }
 
     @GetMapping("/showNewProductForm")
-    public String showNewProductForm(Model model) {
+    public ResponseEntity<String> showNewProductForm(Model model) {
         // create model attribute to bind form data
         ShoppingProduct shoppingProduct = new ShoppingProduct();
         model.addAttribute("shoppingProduct", shoppingProduct);
-        return "new_product";
+        return new ResponseEntity<>("new_product", HttpStatus.OK);
     }
 
     @PostMapping("/saveProduct")
-    public String saveProduct(@ModelAttribute("shoppingProduct") ShoppingProduct shoppingProduct) {
+    public ResponseEntity<String> saveProduct(@ModelAttribute("shoppingProduct") ShoppingProduct shoppingProduct) {
         // save product to database
         shoppingProductService.saveProduct(shoppingProduct);
-        return "redirect:/";
+        return new ResponseEntity<>("redirect:/", HttpStatus.OK);
     }
 
     @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+    public ResponseEntity<String> showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
         // get product from the service
         ShoppingProduct shoppingProduct = shoppingProductService.getProductById(id);
 
         // set product as a model attribute to pre-populate the form
         model.addAttribute("shoppingProduct", shoppingProduct);
-        return "update_product";
+        return new ResponseEntity<>("update_product", HttpStatus.OK);
     }
 
     @GetMapping("/deleteShoppingProduct/{id}")
-    public String deleteShoppingProduct(@PathVariable(value = "id") long id) {
+    public ResponseEntity<String> deleteShoppingProduct(@PathVariable(value = "id") long id) {
         // call delete product method
         this.shoppingProductService.deleteProduct(id);
-        return "redirect:/";
+        return new ResponseEntity<>("redirect:/", HttpStatus.OK);
     }
 
 }
